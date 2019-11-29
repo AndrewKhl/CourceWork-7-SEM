16f39c7fbca1640570a061afb9e8c822c9c9327a
palina.sh <palina.sh@gmail.com>
2019-11-30 00:19:06 +0300

    Added Common styles for button, textBlock and textBox

--- MyHotel/ViewModels/MainWindowViewModel.cs
+++ MyHotel/ViewModels/MainWindowViewModel.cs
@@ -43,7 +43,6 @@ namespace MyHotel
             {
                 DataContext = loginViewModel,
                 Owner = _mainWindow,
-                WindowStartupLocation = WindowStartupLocation.CenterOwner,
             };
 
             loginDialog.ShowDialog();
