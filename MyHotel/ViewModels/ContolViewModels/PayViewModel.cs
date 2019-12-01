bf10a741ee995d2ab4b75ce5b7083199451cbc01
palina.sh <palina.sh@gmail.com>
2019-12-01 23:30:06 +0300

    Added Pay Dialog

---
+++ MyHotel/ViewModels/ContolViewModels/PayViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHotel
+{
+    public class PayViewModel : BaseViewModel
+    {
+        private long _cardNumber;
+        private int _dateExpiration;
+        private int _monthExpiration;
+        private string _cardholderName;
+        private int _cvs;
+
+        public long CardNumber
+        {
+            get => _cardNumber;
+            set
+            {
+                _cardNumber = value;
+                NotifyPropertyChanged(() => CardNumber);
+            }
+        }
+
+        public int DateExpiration
+        {
+            get => _dateExpiration;
+            set
+            {
+                _dateExpiration = value;
+                NotifyPropertyChanged(() => DateExpiration);
+            }
+        }
+
+        public int MonthExpiration
+        {
+            get => _monthExpiration;
+            set
+            {
+                _monthExpiration = value;
+                NotifyPropertyChanged(() => MonthExpiration);
+            }
+        }
+
+        public string CardholderName
+        {
+            get => _cardholderName;
+            set
+            {
+                _cardholderName = value;
+                NotifyPropertyChanged(() => CardholderName);
+            }
+        }
+
+        public int Cvs
+        {
+            get => _cvs;
+            set
+            {
+                _cvs = value;
+                NotifyPropertyChanged(() => Cvs);
+            }
+        }
+
+        public PayViewModel(IShellViewModel shellViewModel) : base(shellViewModel)
+        {
+
+        }
+    }
+}
