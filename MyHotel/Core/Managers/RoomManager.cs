5f45eef10c2f048d7d029369f29e34f2f8726659
RedLeonFire <RedLeonFire@yandex.by>
2019-12-01 16:45:04 +0300

    Refactoring

--- MyHotel/Core/Managers/RoomManager.cs
+++ MyHotel/Core/Managers/RoomManager.cs
@@ -16,7 +16,7 @@ namespace MyHotel.Core
 
         public DbSet<LivingRoom> LivingRooms { get; set; }
 
-        public Room TryFindRoom(int id) => LivingRooms.Where(u => u.Id == id).FirstOrDefault();
+        public LivingRoom TryFindRoom(int id) => LivingRooms.Where(u => u.Id == id).FirstOrDefault();
 
         public void InitialCommit()
         {
