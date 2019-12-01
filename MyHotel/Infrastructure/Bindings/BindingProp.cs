cf5f301f42f0defe4793965f27425c5e7d4e6089
RedLeonFire <RedLeonFire@yandex.by>
2019-12-01 17:19:15 +0300

    Custom Binding has been added

---
+++ MyHotel/Infrastructure/Bindings/BindingProp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MyHotel
+{
+    public class BindingProp : Binding
+    {
+        public BindingProp() : base()
+        {
+            SettingsOn();
+        }
+
+        public BindingProp(string path) : base(path)
+        {
+            SettingsOn();
+        }
+
+        private void SettingsOn()
+        {
+            UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            ValidatesOnDataErrors = true;
+            ValidatesOnExceptions = true;
+            ValidatesOnNotifyDataErrors = true;
+        }
+    }
+}
